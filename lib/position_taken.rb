7dbb02ba7e731091de2c2819a33c207212ce59e4
Arjun Nair <nair.arjun47@yahoo.com>
2019-09-11 05:51:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
-def position_taken?()
-  if board == " "
+def position_taken?(board, index)
+  if board == " " || board == "" || board == nil
     return false
   else
     return true
